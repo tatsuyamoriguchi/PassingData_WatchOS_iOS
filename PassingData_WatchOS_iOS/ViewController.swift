a9ce748dc06c71614bc68e791398e23dbe685fc4
Brian Moriguchi <brian@beckos.com>
2020-09-15 16:16:10 -0700

    Initial Commit

---
+++ PassingData_WatchOS_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData_WatchOS_iOS
+//
+//  Created by Tatsuya Moriguchi on 9/15/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
