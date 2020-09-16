9f420ed63363067366ad6374e3d4993ae9d993e0
Brian Moriguchi <brian@beckos.com>
2020-09-16 00:51:27 -0700

    Added UI properties and code to obtain accelerometer data from Apple Watch

--- PassingData_WatchOS_iOS WatchKit Extension/InterfaceController.swift
+++ PassingData_WatchOS_iOS WatchKit Extension/InterfaceController.swift
@@ -8,12 +8,32 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
 
+    let motion = CMMotionManager()
+    var timer = Timer()
+    var intervalN = 10.0
+    
+    @IBOutlet weak var intervalValueLabel: WKInterfaceLabel!
+    @IBOutlet weak var xValueLabel: WKInterfaceLabel!
+    @IBOutlet weak var yValueLabel: WKInterfaceLabel!
+    @IBOutlet weak var zValueLabel: WKInterfaceLabel!
+    
+    
+    @IBAction func startButtonAction() {
+      getAccelerometerData(sliderValue: intervalN)
+  
+    }
+    
+    @IBAction func endButtonAction() {
+          motion.stopAccelerometerUpdates()
+    }
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        intervalValueLabel.setText(String(intervalN))
         
         // Configure interface objects here.
     }
@@ -26,6 +46,34 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    
+    func getAccelerometerData(sliderValue: Double) {
+        // Check to see if the accelerometer hardware is available.
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = sliderValue/60.0 // 60 Hz update frequency
+            self.motion.startAccelerometerUpdates()
+            
+            // Configure a timer to fetchthe data.
+            self.timer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: {(timer) in
+                // Get the accelerometer data.
+                if let data = self.motion.accelerometerData {
+                    let x = data.acceleration.x
+                    let y = data.acceleration.y
+                    let z = data.acceleration.z
+                    
+                    // Display the accelerometer data on UI.
+                    self.xValueLabel.setText(String(x))
+                    self.yValueLabel.setText(String(y))
+                    self.zValueLabel.setText(String(z))
+                }
+            })
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(self.timer, forMode: .default)
+        }
+        
     }
 
 }
